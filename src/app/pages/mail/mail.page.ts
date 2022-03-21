c3177587fb1d6eb4a495a86a8704ad49fdae4fd1
Tenituun <tuunanen.teemu@gmial.com>
2022-03-21 13:33:01 +0200

    Teemun muutokset

--- src/app/pages/mail/mail.page.ts
+++ src/app/pages/mail/mail.page.ts
@@ -7,13 +7,38 @@ import { HttpClient } from  '@angular/common/http';
   styleUrls: ['./mail.page.scss'],
 })
 export class MailPage implements OnInit {
+	emails = [];
 
 	constructor(private http: HttpClient) { }
 
 	ngOnInit() {
 		this.http.get<any[]>("./assets/data.json").subscribe(res => {
-			console.log(res)
+			this.emails = res.map(email => {
+				email.color = this.intToRGB(this.hashCode(email.from));
+				return email;
+			});
+			console.log(this.emails);
 		});
+	}
+
+	private hashCode(str) {
+		var hash = 0;
+		for (var i = 0; i < str.length; i++) {
+			hash = str.charCodeAt(i) + ((hash << 5) - hash);
+		}
+		return hash;
+	}
+
+	private intToRGB(i) {
+		var c = (i & 0x00FFFFFF)
+		.toString(16)
+		.toUpperCase();
+
+		return '#' + '00000'.substring(0, 6 - c.length) + c;
+	}
+
+	openAccount() {
+
 	}
 
 }
